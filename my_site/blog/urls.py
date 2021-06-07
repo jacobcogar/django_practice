cf0c5b66f0688944be387a1fa16ad4a849abc9f0
JacobCogar <jacoblcogar@gmail.com>
2021-06-06 20:58:11 -0500

    blog exercise start

---
+++ my_site/blog/urls.py
@@ -0,0 +1,12 @@
+# from blog.views import function 
+from django.urls import path # takes path after domain and maps it
+from . import views
+
+
+urlpatterns = [
+    path('', views.starting_page, name='starting-page'),
+    path('posts', views.posts, name='posts-page'),
+    path('posts/<slug:slug>', views.post_detail, name='post-detail-page') #/posts/my-first-post
+]
+
+
---
+++ my_site/blog/views.py
@@ -0,0 +1,16 @@
+from django.http.response import Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
+from django.urls import reverse
+
+# Create your views here.
+
+
+def starting_page(request):
+    pass
+
+def posts(requests):
+    pass
+
+def post_detail(request):
+    pass
