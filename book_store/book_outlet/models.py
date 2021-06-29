908a06edff90ff99758c14b8eb8c40814503114b
Jacob Cogar <JCogar@stepan.com>
2021-06-28 20:20:57 -0500

    completed relationships

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,9 +6,34 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}, {self.code}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+    
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    # favorite_created_book =   # One-to-One relationship
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -20,20 +45,19 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books") # or PROTECT or SETNULL
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books") # or PROTECT or SETNULL; ForeignKey used for many-to-one relationship
     is_bestselling_book = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True) # Harry Potter 1 => harry-potter-1
+    published_countries = models.ManyToManyField(Country, )
 
     # id = models.AutoField()  Can be used to increment but not needed. Automatically done.
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
 
-
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.title)
     #     super().save(*args, **kwargs)
-
 
     def __str__(self): # method exists for every class in python. Can be overriden for changes to display
         return f"{self.title} ({self.rating})"
